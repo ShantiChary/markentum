f607ba76f5705fecf6cf6d86a190ae5073cd4adc
Shanti Chary <shantichary@gmail.com>
2021-04-26 17:58:17 -0700

    Create taxonomy templates to display content including ACF custom field for each location
    Created taxonomy templates for each location to display employees per location. Added a custom field for employee title. Updated
    menu to include the taxonomies by enabling the 'Locations' taxonomy in the 'Screen Options', so that the user is able to navigate to the
     appropriate pages via the menu.

---
+++ wp-content/themes/twentytwentyone/taxonomy-location-category-location-one.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+			<header class="page-header">
+				<?php
+					echo '<h1>';
+					echo single_term_title();
+					echo '</h1>';
+
+				?>
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+			<div>
+			<article class="location-one">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php
+
+					echo '<p>'; ?>
+				
+						<!-- Get and display Employee Title Custom field -->
+					<?
+						$field_name = "employee_title";
+						$field = get_field_object($field_name); ?>
+						<?php if( !empty(get_field('employee_title')) ): ?>
+							<p><?php the_field('employee_title'); ?></p>
+						<?php endif;
+
+					echo '</p>';
+
+					echo '<h4>';
+					the_title();
+					echo '</h4>';
+					
+					the_post_thumbnail('large', array( 'class' => 'aligncenter' ) );
+
+				?>
+			<?php endwhile; ?>
+		</article>
+		</div>
+
+		<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+	</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
